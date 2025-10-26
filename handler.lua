275b98e35d26f33c11376ddfc36d3858470dfe71
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-10-26 06:33:12 -0400

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -3,6 +3,47 @@ local Players = cloneref and cloneref(game:GetService("Players")) or game:GetSer
 local TeleportService = cloneref and cloneref(game:GetService("TeleportService")) or game:GetService("TeleportService")
 local LocalPlayer = Players.LocalPlayer
 local url = "https://raw.githubusercontent.com/EnterpriseExperience/FakeChatGUI/refs/heads/main/users.json"
+local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/main/Notification_Lib.lua"))()
+getgenv().NotifyLib = NotifyLib
+
+local function retrieve_executor()
+   local name
+   if identifyexecutor then
+      name = identifyexecutor()
+   end
+   return { Name = name or "Unknown Executor" }
+end
+
+local function identify_executor()
+   local executorDetails = retrieve_executor()
+   return tostring(executorDetails.Name)
+end
+
+local executor_string = identify_executor()
+
+local function executor_contains(substr)
+   if type(executor_string) ~= "string" then
+      return false
+   end
+
+   return string.find(string.lower(executor_string), string.lower(substr), 1, true) ~= nil
+end
+
+if not getgenv().notify then
+    if executor_contains("LX63") then
+        function notify(notif_type, msg, duration)
+            NotifyLib:StarterGui_Notify(tostring(notif_type), tostring(msg), tonumber(duration))
+        end
+        wait(0.1)
+        getgenv().notify = notify
+    else
+        function notify(notif_type, msg, duration)
+            NotifyLib:External_Notification(tostring(notif_type), tostring(msg), tonumber(duration))
+        end
+        wait(0.1)
+        getgenv().notify = notify
+    end
+end
 
 local success, result = pcall(function()
     local data = game:HttpGet(url)
@@ -45,7 +86,7 @@ end
 
 for name, entry in pairs(users) do
    if not is_expired(entry) and Players:FindFirstChild(name) then
-      print("Warning", ("Blacklisted user in server: %s (%s)"):format(name, entry.reason or "No reason"))
+      getgenv().notify("Warning", ("Blacklisted user in server: %s (%s)"):format(name, entry.reason or "No reason"))
    end
 end
 
