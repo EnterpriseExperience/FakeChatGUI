4c1f13c292d29c0415deb240517874e5e70da225
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-12-13 08:05:29 -0500

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -1,5 +1,8 @@
 local http_requesting = request or http_request or (syn and syn.request) or (http and http.request) or (fluxus and fluxus.request)
 local httpreq = http_requesting
+getgenv().ConstantUpdate_Checker_Live = true
+local Raw_Version = "V7.2.9"
+local Script_Version = tostring(Raw_Version).."-LifeAdmin"
 
 function Notify(message, duration)
    local CoreGui = cloneref and cloneref(game:GetService("CoreGui")) or game:GetService("CoreGui")
@@ -66,27 +69,6 @@ function Notify(message, duration)
       end
    end)
 end
-
-task.spawn(function()
-   while getgenv().ConstantUpdate_Checker_Live do
-      task.wait(0.5)
-      local success, latestVersionInfo = pcall(function()
-         local versionJson = game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/main/Script_Versions_JSON?cachebust=" .. tick())
-         return HttpService:JSONDecode(versionJson)
-      end)
-
-      if success and latestVersionInfo then
-         if Script_Version ~= latestVersionInfo.LifeTogether_Admin_Version then
-            getgenv().ConstantUpdate_Checker_Live = false
-            Notify("[LIFE TOGETHER ADMIN]: do NOT rejoin! An update is now out! Update version: "..tostring(latestVersionInfo.LifeTogether_Admin_Version).." | re-executing automatically...", 30)
-            getgenv().LifeTogetherRP_Admin = false
-            wait(3)
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP_Admin.lua'))()
-            break
-         end
-      end
-   end
-end)
 
 local function normalize_response(res)
    local status = res.StatusCode or res.statusCode or res.status or res.Status
@@ -230,4 +212,25 @@ task.spawn(function()
             end)
         end
     end
+end)
+
+task.spawn(function()
+   while getgenv().ConstantUpdate_Checker_Live do
+      task.wait(0.5)
+      local success, latestVersionInfo = pcall(function()
+         local versionJson = game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/main/Script_Versions_JSON?cachebust=" .. tick())
+         return HttpService:JSONDecode(versionJson)
+      end)
+
+      if success and latestVersionInfo then
+         if Script_Version ~= latestVersionInfo.LifeTogether_Admin_Version then
+            getgenv().ConstantUpdate_Checker_Live = false
+            Notify("[LIFE TOGETHER ADMIN]: do NOT rejoin! An update is now out! Update version: "..tostring(latestVersionInfo.LifeTogether_Admin_Version).." | re-executing automatically...", 30)
+            getgenv().LifeTogetherRP_Admin = false
+            wait(3)
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP_Admin.lua'))()
+            break
+         end
+      end
+   end
 end)
