070624719d9a0af4f055a4490ddf7624145a88c1
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-01-22 16:06:39 -0500

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -1,7 +1,7 @@
 local http_requesting = request or http_request or (syn and syn.request) or (http and http.request) or (fluxus and fluxus.request)
 local httpreq = http_requesting
 getgenv().ConstantUpdate_Checker_Live = true
-local Raw_Version = "V7.8.0 (HAHAHAHA😈)"
+local Raw_Version = "V7.9.3 (WE ARE SO BACK!😈)"
 local Script_Version = getgenv().Script_Version or getgenv().Script_Version_GlobalGenv
 
 function Notify(message, duration)
